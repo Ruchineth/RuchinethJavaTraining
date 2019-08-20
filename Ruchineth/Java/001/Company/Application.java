f44bcaab729ce65deb3cf9b3498aa13ae688c731
Ruchineth <35953159+Ruchineth@users.noreply.github.com>
2019-08-20 07:55:28 +0530

    Add files via upload
    To set the class path used "SET CLASSPATH=.;E:\Ruchineth\Java\001\Company.:E:\Ruchineth\Java\001\Employee"

---
+++ Ruchineth/Java/001/Company/Application.java
@@ -0,0 +1,9 @@
+public class Application{
+	public static void main(String[] args){
+		Company cmp=new Company();
+		Employee emp=new Employee();
+		System.out.println(cmp.getName());
+		System.out.println(emp.getName());
+	}
+}
+
---
+++ Ruchineth/Java/001/Employee/Application.java
@@ -0,0 +1,6 @@
+public class Application{
+	public static void main(String[]args){
+		Employee emp=new Employee();
+		System.out.println(emp.getName());
+	}
+}
