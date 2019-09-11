3a3b5f00825cd84ad327452be766cadd5917327c
SacheeSubasingha <ruchineth@gmail.com>
2019-09-11 18:43:19 +0530

    Simple auth server

---
+++ demo/src/main/java/ruchineth/demo/conf/UserConfiguretion.java
@@ -0,0 +1,16 @@
+package ruchineth.demo.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UserConfiguretion extends GlobalAuthenticationConfigurerAdapter {
+    PasswordEncoder passwordEncoder= PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("Dinushan").password(passwordEncoder.encode("dinushan")).roles("USER","ADMIN","MANAGER").authorities("CAN_READ","CAN_WRITE","CAN_DELETE").and().withUser("Kusal").password(passwordEncoder.encode("kusal")).roles("USER").authorities("CAN_READ","CAN_WRITE");
+    }
+}
